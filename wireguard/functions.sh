7057e5614bdf1df174fcc72facafb3b94b5550e4
Jörg Thalheim <joerg@higgsboson.tk>
2016-08-13 20:36:10 +0000

    more wireguard fixes

--- wireguard/functions.sh
+++ wireguard/functions.sh
@@ -13,10 +13,10 @@ add() {
   ip link add dev "$name" type wireguard
 
   echo "$PRIVATE_KEY" | wg set "$name" \
+    $([[ -n "$listen_port" ]] && echo "listen-port $listen_port") \
     private-key /dev/stdin \
     peer "$public_key" \
     allowed-ips "0.0.0.0/0,::/0" \
-    $([[ -n "$listen_port" ]] && echo "listen-port $listen_port") \
     $([[ -n "$endpoint" ]] && echo "endpoint $endpoint")
 
   ip link set dev "$name" up
@@ -28,12 +28,18 @@ add() {
       ip addr add "$OWN_IP4" dev "$name"
     fi
   fi
-  [ -n "$OWN_IP6" ] && ip addr add "$OWN_IP6" dev "$name"
-  [ -n "$ip6" ] && ip route add "$ip6" dev "$name"
+  if [ -n "$OWN_IP6" ]; then
+    ip addr add "$OWN_IP6" dev "$name"
+  fi
+  if [ -n "$ip6" ]; then
+    ip route add "$ip6" dev "$name"
+  fi
 }
 del() {
   name="$2"
-  ip link | grep -q "$name" && ip link del "$name" || true
+  if ip link | grep -q "$name"; then
+    ip link del "$name"
+  fi
 }
 
 case "${1:-}" in
